fb4262bf30eee8091a4955c9ceca8c78712596d6
CyanPiano <cyanp0515@gmail.com>
2022-01-30 02:12:58 -0500

    Format + cleanup vue stuff

---
+++ src/components/home/LinkInput.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>HI</h2>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "LinkInput",
+    props: {
+        msg: String,
+    },
+});
+</script>
